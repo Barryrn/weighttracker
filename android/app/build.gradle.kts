376a0f1ee7f2902e14fcd72f5f4410e2fc2d2b04
Barry <azihuwabarry0@gmail.com>
2025-09-11 09:07:04 +0200

    Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.BarryAzihuwaDev.BodyTrack"
+        applicationId = "com.BarryAzihuwaRelease.BodyTrack"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26  // Keep this for health plugin compatibility
